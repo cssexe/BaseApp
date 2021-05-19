f03d549efb79ddbff54baad68a545f796d2a8bc6
Mohammed Nasser <ab07daa@gmail.com>
2021-05-19 13:46:09 +0300

    prepare Base Class

---
+++ app/src/main/java/com/moha/baseapp/Base.kt
@@ -0,0 +1,19 @@
+package com.moha.baseapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class Base <VB: ViewBinding> : AppCompatActivity () {
+
+    abstract val bindingInflater:(LayoutInflater) -> VB
+    private lateinit var _binding: ViewBinding
+    protected val binding = _binding as VB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = bindingInflater(layoutInflater)
+        setContentView(_binding.root)
+    }
+}
--- app/src/main/java/com/moha/baseapp/MainActivity.kt
+++ app/src/main/java/com/moha/baseapp/MainActivity.kt
@@ -2,10 +2,11 @@ package com.moha.baseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.moha.baseapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : Base<ActivityMainBinding>() {
+    override val bindingInflater: (LayoutInflater) -> ActivityMainBinding = ActivityMainBinding::inflate
+
+
 }
